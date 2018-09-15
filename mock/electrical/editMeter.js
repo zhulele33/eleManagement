8124741c74bad618fac312289ffc98621cf71669
zhuchunle001 <zhuchunle001@lianjia.com>
2018-09-15 21:01:54 +0800

    列表页

---
+++ mock/electrical/editMeter.js
@@ -0,0 +1,6 @@
+module.exports = {
+    code:200,
+    data:{},
+    "msg": "成功"
+}
+
---
+++ mock/electrical/renterRechargeRecord.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "code":200,
+    "data":{
+        "endRow":1,
+        "firstPage":1,
+        "hasNextPage":false,
+        "hasPreviousPage":false,
+        "isFirstPage":true,
+        "isLastPage":true,
+        "lastPage":1,
+        "list":[
+            {
+                "createTime":"2018-09-16 07:23:49",
+                "id":1,
+                "isDel":1,
+                "meterId":"1",
+                "price":0,
+                "rechargePrice":1,
+                "updateTime":"2018-09-16 07:24:09"
+            }
+        ],
+        "navigateFirstPage":1,
+        "navigateLastPage":1,
+        "navigatePages":8,
+        "navigatepageNums":[
+            1
+        ],
+        "nextPage":0,
+        "pageNum":1,
+        "pageSize":10,
+        "pages":1,
+        "prePage":0,
+        "size":1,
+        "startRow":1,
+        "total":1
+    },
+    "msg":"成功"
+}
